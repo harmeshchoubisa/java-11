5a8b321b43d720f546f45e71fb044bd0eb1c4694
Choubisa, H. (Harmesh) <harmesh.choubisa@ing.com>
2021-03-20 15:59:06 +0100

    added concept of dafault package

---
+++ src/main/java/com/sogeti/specialpackage/RedundantImport.java
@@ -0,0 +1,20 @@
+package com.sogeti.specialpackage;
+
+import java.lang.System;
+import java.lang.*;
+import java.util.Random;
+import java.util.*;
+
+/**
+ * This class demonstrates the concept of default package
+ * i.e java.lang is a default package being imported in all classes
+ * The above imports on Line 3, 4 and 6 are redundant.
+ */
+public class RedundantImport {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+
+    }
+}
