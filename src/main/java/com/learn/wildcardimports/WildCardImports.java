5764c8b86967630c5b18253bb605942039e59ebb
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-11 09:29:26 +0100

    wildcard imports works only with class names

--- src/main/java/com/learn/wildcardimports/WildCardImports.java
+++ src/main/java/com/learn/wildcardimports/WildCardImports.java
@@ -4,6 +4,13 @@ import java.nio.file.Paths;
 import java.nio.file.Files;
 // import java.nio.*.*; // NO GOOD - you can only have one wildcard.
 
+//import java.nio.*;         // NO GOOD - a wildcard only matches
+                             // class names, not "file.Files"
+
+//import java.nio.file.Paths.*; // NO GOOD - you cannot import methods
+                                // only class names
+                                // unless static imports are used
+
 
 public class WildCardImports {
     public static void main(String[] args) {
