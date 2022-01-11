451a7d24439b16e27f46514a1a9ec7fc9caec2be
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-11 08:32:38 +0100

    added single file concept to run only with java command

---
+++ src/main/java/com/learn/programswithoutdide/SingleFileZoo.java
@@ -0,0 +1,13 @@
+// If there is single file program ie A file with only one class then we can also use
+// just java command to compile and run simultaneously else we have to always compile first with javac command.
+// ie java SingleFileZoo.java anyString
+
+public class SingleFileZoo {
+
+public static void main(String... args) {
+
+System.out.println("Single file: " +args[0]);
+}
+
+
+}
---
+++ src/main/java/com/learn/programswithoutdide/Zoo.java
@@ -0,0 +1,8 @@
+// The classes that are required to run from terminal and not from IDE
+public class Zoo {
+
+public static void main(String args[]) {
+
+}
+
+}
