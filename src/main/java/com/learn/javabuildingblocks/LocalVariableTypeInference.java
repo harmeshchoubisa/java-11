290e319c4c0af6f34f92ef9732f9874048641f33
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-13 16:36:50 +0100

    var type can be used only for local variables

---
+++ src/main/java/com/learn/javabuildingblocks/LocalVariableTypeInference.java
@@ -0,0 +1,15 @@
+package com.learn.javabuildingblocks;
+
+public class LocalVariableTypeInference {
+
+// var exampleInstanceVarialbe = "Hello"; // DOES NOT COMPILE
+                                          // var can only be used for local variables
+
+public static void main(String args[]) {
+
+var exampleIntType = 5;
+var exampleStringType = "Hello";
+}
+
+
+}
