be45ffbe224c42752cdb4918b711ad637628d50b
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-13 15:56:32 +0100

    concept of local instance and class variable initialization

---
+++ src/main/java/com/learn/javabuildingblocks/InitializeVariable.java
@@ -0,0 +1,21 @@
+package com.learn.javabuildingblocks;
+
+public class InitializeVariable {
+
+private static String exampleClassVariable; // Not required to initialize as given default
+                                            // value as soon it is declared
+
+private int exampleInstanceVariale; // Not required to intialize as given default 
+                                    // value as soon it is declared
+
+public static void main(String[] args) {
+
+int x = 5;
+int y;
+// int z = x+y; // DOES NOT COMPILE
+                // Local variables need to be initialized before use
+
+
+}
+
+}
