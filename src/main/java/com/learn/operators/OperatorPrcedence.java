d185da16b41c80aa1d5a81003752b970602f7a89
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-14 15:07:38 +0100

    concept of operator precedence

---
+++ src/main/java/com/learn/operators/OperatorPrcedence.java
@@ -0,0 +1,15 @@
+package com.learn.operators;
+
+public class OperatorPrcedence {
+
+public static void main (String... args) {
+
+int lion = 3;
+int tiger = ++lion * 5 / lion--;
+System.out.println("lion is " +lion);
+System.out.println("tiger is" +tiger);
+
+}
+
+
+}
