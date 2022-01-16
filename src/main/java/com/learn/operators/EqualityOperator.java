6e7a431bc05390332325ccf0a13034ae3f99ccd4
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-16 11:52:18 +0100

    concept of equality operator

--- src/main/java/com/learn/operators/EqualityOperator.java
+++ src/main/java/com/learn/operators/EqualityOperator.java
@@ -9,7 +9,6 @@ public static void main (String... args) {
 // Even though all of the variables point to the same file information,
 // only two references, tuesday and wednesday,
 // are equal in terms of == since they point to the same object.
-
 File monday = new File("schedule.txt");
 File tuesday = new File("schedule.txt");
 File wednesday = tuesday;
