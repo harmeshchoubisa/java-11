093b562b8b963f6db31c4d438761ab2a690b1d97
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-17 22:37:55 +0100

    string concatenation with three important rules

---
+++ src/main/java/com/learn/javacoreapis/StringConcatenation.java
@@ -0,0 +1,19 @@
+package com.learn.javacoreapis;
+
+/**
+ * 1. If both operands are numeric, + means numeric addition.
+ * 2. If either operand is a String, + means concatenation.
+ * 3. The expression is evaluated left to right.
+ */
+
+public class StringConcatenation {
+
+    public static void main(String[] args) {
+
+        System.out.println(1 + 2);               // 3
+        System.out.println("a" + "b");           // ab
+        System.out.println("a" + "b" + 3);       // ab3
+        System.out.println(1 + 2 + "c");         // 3c
+        System.out.println("c" + 1 + 2);         // c12
+    }
+}
