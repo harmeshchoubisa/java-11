7113aef41bbd222bf25d90a6a816c264a3a7c0f2
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-17 22:48:43 +0100

    concept of string immutability

---
+++ src/main/java/com/learn/javacoreapis/StringImmutability.java
@@ -0,0 +1,16 @@
+package com.learn.javacoreapis;
+
+/**
+ * In real world scenario setter method converts the class to mutable.
+ * If we want to ensure immutability we can just keep only getter methods.
+ */
+public class StringImmutability {
+
+    public static void main(String[] args) {
+
+        String s1 = "1";
+        String s2 = s1.concat("2");
+        s2.concat("3"); // String class is immutable so s2 will not change/mutate hence output will be 12
+        System.out.println(s2);
+    }
+}
